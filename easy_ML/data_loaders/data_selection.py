12a9cd2e321417a0463d3e690568706628e2d0cf
Turtle24 <aidan.nell@yahoo.com>
2021-02-10 16:06:15 +0100

    Updated data selection

--- easy_ML/data_loaders/data_selection.py
+++ easy_ML/data_loaders/data_selection.py
@@ -1,5 +1,5 @@
 from sklearn import datasets as df
-from PyInquirer import inquirer
+from pick import pick
 
 class DataSelection:
     
@@ -12,17 +12,17 @@ class DataSelection:
     def __repr__(self):
         return f"{self.classification} \n {self.regression}"
 
-    def data_loader(self, model):
-        self.options = self.classification if model == 'clf' else self.regression
-
-        selection_prompt = [inquirer.List('selection',
-                                            message=f'Choose a {self.model} dataset',
-                                            choices=options)]
-        answer = inquirer.prompt(selection_prompt)
-        selection = prompt(answer)
+    def data_loader(self):
+        title = 'Please choose the type of data you want: '
+        options = ['classification', 'regression']
+        # option, index = pick(options, title)
+        selected = pick(options, title, multiselect=True, min_selection_count=1)
+        # follow_up = 'Now choose your dataset: '
+        # next_options = option
+        # selection = 
         # load data
-        print('loading')
-        choice = df.selection
+        # print('loading')
+        # choice = df.selection
 
-        return choice
+        return selected
 
